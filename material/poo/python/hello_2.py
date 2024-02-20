9742cf8762394f88d23942ca95f9831d1ff2ec0e
yostane <1958676+yostane@users.noreply.github.com>
2024-02-20 12:54:27 +0100

    Add some list samples

---
+++ material/poo/python/hello_2.py
@@ -0,0 +1,21 @@
+numbers = [3, 49, 2, -29, 17]
+
+print(len(numbers))
+
+print(numbers[2:4])
+print(numbers[2:])
+print(numbers[:3])
+
+for i in [0, 1, 2, 3, 4]:
+    print(i)
+
+for i in range(5):
+    print(i)
+
+for i in [10, 12, 14]:
+    print(i)
+
+for i in range(10, 100001, 1000):
+    print(i)
+
+print("done")
