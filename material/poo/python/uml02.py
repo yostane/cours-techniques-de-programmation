dda1c9f5dc31db61f1fd44f598444bc360a7ade5
yostane <1958676+yostane@users.noreply.github.com>
2024-03-22 10:40:35 +0100

    Add some uml diagram

---
+++ material/poo/python/uml02.py
@@ -0,0 +1,51 @@
+import datetime
+
+
+class LivingBeing:
+    def __init__(self, name, birth_date) -> None:
+        self.name = name
+        self.birth_date = birth_date
+
+
+class Brain:
+    def __init__(self, volume) -> None:
+        self.volume = volume
+
+
+class Hand:
+    def __init__(self, finger_count) -> None:
+        self.finger_count = finger_count
+
+    def do_thumbs_up(self):
+        print("👍")
+
+
+class Human(LivingBeing):
+    def __init__(
+        self, name, birth_date, social_security_number, height, hands, brain_volume
+    ) -> None:
+        super().__init__(name, birth_date)
+        self.social_security_number = social_security_number
+        self.height = height
+        self.brain = Brain(brain_volume)
+        self.hands = hands
+
+    def walk(self, distance):
+        print(f"🚶 during {distance}")
+
+    def sleep(self, duration):
+        print(f"It's time to 😴 for {duration} minutes")
+
+    def talk(self, sentence):
+        print(f"🗣️: {sentence}")
+
+
+human1_hands = [Hand(3), Hand(12)]
+human1 = Human(
+    "Rémie", datetime.datetime(2003, 8, 15), 1_987_687_686, 175, human1_hands, 2
+)
+
+print("brain volume", human1.brain.volume)
+human1.sleep(10)
+human1.walk(2000)
+human1.talk("hahaha")
